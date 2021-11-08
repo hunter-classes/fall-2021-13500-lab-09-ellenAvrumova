fd31f35f40c7843edb452109818090439cc656fd
Ellen Avrumova <ellenavrumova@Ellens-MacBook-Air.local>
2021-11-08 10:24:27 -0500

    Completed part B

--- funcs.cpp
+++ funcs.cpp
@@ -8,3 +8,13 @@ double length(Coord3D *p) {
     double length = sqrt(pow((*p).x,2) + pow((*p).y,2) + pow((*p).z,2));
     return length;
 }
+
+Coord3D* fartherFromOrigin(Coord3D *p1, Coord3D *p2) {
+    if(length(p1) > length(p2)) {
+        return p1;
+    }
+    else {
+        return p2;
+    }
+    
+}
--- main.cpp
+++ main.cpp
@@ -3,8 +3,18 @@
 #include "funcs.h"
 
 int main() {
+  std::cout << "***PART A***" << std::endl;
   Coord3D pointP = {10, 20, 30};
-    std::cout << length(&pointP) << std::endl;
+  std::cout << length(&pointP) << std::endl;
 
+  std::cout << "***PART B***" << std::endl;
+  Coord3D pointA = {10, 20, 30};
+  Coord3D pointB = {-20, 21, -22};
+  std::cout << "Address of A: " << &pointA << std::endl;
+  std::cout << "Address of B: " << &pointB << std::endl << std::endl;
+
+  Coord3D* ans = fartherFromOrigin(&pointA, &pointB);
+   
+  std::cout << "ans = " << ans << std::endl;
   return 0;
 }
