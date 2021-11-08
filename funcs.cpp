bb104c96a7cbd554b64bb68d72826e80127579b8
Ellen Avrumova <ellenavrumova@Ellens-MacBook-Air.local>
2021-11-08 09:57:36 -0500

    Completed part A

--- funcs.cpp
+++ funcs.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
+#include <math.h>
+#include "cmath"
+#include "coord3d.h"
 #include "funcs.h"
 
-// add functions here
-
+double length(Coord3D *p) {
+    double length = sqrt(pow((*p).x,2) + pow((*p).y,2) + pow((*p).z,2));
+    return length;
+}
--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
-
-
+#include "coord3d.h"
 #include "funcs.h"
 
-int main()
-{
+int main() {
+  Coord3D pointP = {10, 20, 30};
+    std::cout << length(&pointP) << std::endl;
+
   return 0;
 }
